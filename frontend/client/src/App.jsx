2b199ff4386274afc6b22dc810f43ed09e3ac984
pankaj-2503 <pankajpj3103@gmail.com>
2024-03-20 18:20:12 +0530

    file sharing using mern

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,44 +1,61 @@
-
-import './App.css'
-import { useRef,useState,useEffect } from 'react';
+import "./App.css";
+import { useRef, useState, useEffect } from "react";
 import { IoCloudUploadSharp } from "react-icons/io5";
-import {uploadFile} from "./services/api";
+import { uploadFile } from "./services/api";
 
 function App() {
-  const [file,setFile]=useState("");
+  const [file, setFile] = useState("");
+  const [result,setResult]=useState("");
+
   // on clicking button open folder logic
-  const fileInputRef=useRef();
-  const openfile=()=>{
-      fileInputRef.current.click();
-  }
-  useEffect(()=>{
-      const getFiles=async()=>{
-        if(file){
-          const data=new FormData();
-          data.append("name",file.name);
-          data.append("file",file);
-          let response=await uploadFile(data);
-        }
+  const fileInputRef = useRef();
+
+  const openfile = () => {
+    fileInputRef.current.click();
+  };
+
+  useEffect(() => {
+    const getFiles = async () => {
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+        let response = await uploadFile(data);
+        setResult(response.path);
       }
-      getFiles();
-  },[file])
+    };
+    getFiles();
+  }, [file]);
   console.log(file);
+  
   return (
     <>
-     <div>
-       <div className="flex justify-center">
-          <img src="" alt="" />
-          <div className="bg-gray-700 w-[80%] h-[60vh] rounded-xl  justify-center mx-auto my-12 md:w-[50%] md:h-[70vh]">
-            <h2 className="text-white text-2xl my-8 font-semibold">Share files</h2>
-            <IoCloudUploadSharp className='w-9 h-9 text-white mx-auto my-4 hover:text-zinc-400'/>
-            <button className='text-white bg-indigo-600 rounded-xl p-3 text-xs font-mono my-4 hover:bg-indigo-700 duration-200 ease-in-out' onClick={openfile}>Upload file to generate link</button>
-             {/* input is used to open file using button on click  */}
-            <input type='file' ref={fileInputRef} className='hidden' onChange={(e)=>setFile(e.target.files)}/>
+      <div>
+        <div className="flex justify-center ">
+          <div className="bg-gray-700 w-[80%] h-[60vh] rounded-xl flex justify-center flex-col  items-center my-12 md:w-[50%] md:h-[70vh]">
+            <h2 className="text-white text-3xl my-6 text-center font-semibold">
+              Share files
+            </h2>
+            <IoCloudUploadSharp className="w-9 h-9 text-white mx-auto my-6 hover:text-zinc-400" />
+            <button
+              className="text-white mx-auto  bg-indigo-600 rounded-xl p-3 text-xs font-mono my-4 hover:bg-indigo-700 duration-200 ease-in-out"
+              onClick={openfile}
+            >
+              Upload file to generate link
+            </button>
+            {/* input is used to open file using button on click  */}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <a href={result} target="_blank">{result}</a>
           </div>
-       </div>
-     </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const express=require("express");
-const app=express();
+import express from 'express';
+import cors from 'cors';
+import {router} from './routes/routes.js';
+import {DBConnection} from './database/db.js';
+import dotenv from 'dotenv';
 
-const port=8000;
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    console.log("hello world");
-})
+const app = express();
 
-app.listen(()=>{
-    console.log(`App is listening at port ${port}`);
-})
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', router);
+
+const PORT = process.env.PORT || 8000;
+
+DBConnection();
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
